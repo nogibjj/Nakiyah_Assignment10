851517b823e163143ddbe1ebef7a794a97779777
Nakiyah Dhariwala <nakiyahdhariwala@Nakiyahs-MacBook-Pro-2.local>
2024-11-09 16:24:05 -0500

    second

--- Makefile
+++ Makefile
@@ -1,27 +1,36 @@
 install:
-	pip install --upgrade pip && pip install -r Requirements.txt
-
-format:
-	black *.py
-	
-lint:
-	ruff check *.py
+	pip install --upgrade pip &&\
+		pip install -r Requirements.txt
 
 test:
-	python3 -m pytest -vv --nbval -cov=mylib -cov=main_file test_*.py *.ipynb
-	
-all: install format lint test
+	python -m pytest -vv -cov=mylib test_*.py
+
+format:	
+	black *.py 
+
+lint:
+	#disable comment to test speed
+	#pylint --disable=R,C --ignore-patterns=test_.*?py *.py mylib/*.py
+	#ruff linting is 10-100X faster than pylint
+	ruff check *.py mylib/*.py
+
+container-lint:
+	docker run --rm -i hadolint/hadolint < Dockerfile
+
+refactor: format lint
+
+deploy:
+	#deploy goes here
+		
+all: install lint test format deploy
 
 generate_and_push:
-	# Create the markdown file
-	python main.py
-
 	# Add, commit, and push the generated files to GitHub
 	@if [ -n "$$(git status --porcelain)" ]; then \
 		git config --local user.email "action@github.com"; \
 		git config --local user.name "GitHub Action"; \
 		git add .; \
-		git commit -m "Add generated plot and report"; \
+		git commit -m "Add output log"; \
 		git push; \
 	else \
 		echo "No changes to commit. Skipping commit and push."; \
--- main.py
+++ main.py
@@ -22,15 +22,18 @@ def main():
     df = readData(spark, file_path)
     
     # Generate summary statistics for a list of columns (you can specify the columns you want here)
-    stats = summaryStatistics(df, columns=["YEAR", "GoogleKnowlege_Occupation"])
+    stats = summaryStatistics(df, columns=["Value for money rank", "Salary percentage increase", "Overall satisfaction **"])
     print(stats)  # You can log or handle stats however you like
     
     # Clean and sort data (example: sorting by "YEAR" and selecting top 5 entries)
-    cleaned_data = cleanData(df, ColToSort="YEAR", Columns=["YEAR", "GoogleKnowlege_Occupation"], RanksRequired=5)
+    cleaned_data = cleanData(df, ColToSort="#", Columns=["#", "School Name", "International students (%)", "International faculty (%)",
+                                                         "Value for money rank", "Career progress rank", "Careers service rank"], 
+                             RanksRequired=5)
+    
     cleaned_data.show()
 
     # Generate a pie chart (example: pie chart for the 'GoogleKnowlege_Occupation' column)
-    PiePlot(df, col="YEAR", labels_col="GoogleKnowlege_Occupation")
+    PiePlot(cleaned_data, col="International students (%)", labels_col="School Name")
     
     # End Spark session
     end_spark(spark)
